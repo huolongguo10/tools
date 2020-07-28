971546ff4fcfcc65a8cc579e0eed47acfa65fe15
huolongguo10 <bjcph6@sina.com>
2020-07-28 12:19:25 +0800

    first

---
+++ onetool/kernel、/math.cpp
@@ -0,0 +1,8 @@
+#include"pch.h"
+#include<string.h>
+#include<math.h>
+#include"functions.h"
+int calc(char* str) {
+	int strlength = strlen(str);
+	return 0;
+}
---
+++ onetool/kernel、/string.cpp
@@ -0,0 +1,25 @@
+#include"pch.h"
+/*example
+// MathLibrary.h - Contains declarations of math functions
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+// The Fibonacci recurrence relation describes a sequence F
+// where F(n) is { n = 0, a
+//               { n = 1, b
+//               { n > 1, F(n-2) + F(n-1)
+// for some initial integral values a and b.
+// If the sequence is initialized F(0) = 1, F(1) = 1,
+// then this relation produces the well-known Fibonacci
+// sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+// Initialize a Fibonacci relation sequence
+// such that F(0) = a, F(1) = b.
+// This function must be called before any other function.
+extern "C" MATHLIBRARY_API void fibonacci_init(
+    const unsigned long long a, const unsigned long long b);*/
