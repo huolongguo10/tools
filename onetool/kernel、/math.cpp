3a3651a6051ecda65fe176f7ee19bc7257a0ef50
huolongguo10 <bjcph6@sina.com>
2020-08-01 16:37:16 +0800

    update

--- onetool/kernel、/math.cpp
+++ onetool/kernel、/math.cpp
@@ -2,7 +2,19 @@
 #include<string.h>
 #include<math.h>
 #include"functions.h"
+#include<stdlib.h>
+#define ISCHAR 0
+#define ISNUM 1
+int isinarray(char ch, char* str) {
+
+}
 int calc(char* str) {
 	int strlength = strlen(str);
+	char* savechar;
+	int* savenum;
+	BOOL* ctrl = (BOOL *)malloc(strlength - 1);
+	for (int i=0;; i++) { 
+		
+	}
 	return 0;
 }
